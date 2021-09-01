e85acdc7ac71450c6d355e4cddf6e10b4e9a0855
Romuald THION <romuald.thion@univ-nc.nc>
2021-09-02 09:38:42 +1100

    génération à fenêtre

--- graphize.py
+++ graphize.py
@@ -32,12 +32,27 @@ if __name__ == "__main__":
     logger.setLevel(logging.INFO)
 
 
-def generate(pairs, min_threshold=2, max_threshold=5):
+def extend_matrix_to_nx(matrix):
+    """etend un dict[mot, dict[mot, int]] en dict[mot, dict[mot, dict[attr, int]]]
+    avec un niveau suppl de dict pour l'attribut "weight" que nx demande"""
+    return {
+        row_word: {col_word: {"weight": matrix[row_word][col_word]} for col_word in matrix[row_word]}
+        for row_word in matrix
+    }
+
+
+def generate(pairs, /, thresholds=None, max_window_widths=None):
     """Genère un ensemble de graphes
 
     Pour chaque paire (carte, thesaurus) de pairs, génère (carte_base, carte_mere)
     puis pour chacune de ces paires, génère un graph pour chaque niveau de threshold entre min et max (inclus)
     """
+    # gère les arguments par défaut
+    if thresholds is None:
+        thresholds = list(range(2, 6))
+    if max_window_widths is None:
+        max_window_widths = [None]
+
     for (carte_file, thesaurus_file) in pairs:
         logger.info(f"carte_file = {carte_file}")
         logger.info(f"thesaurus_file = {thesaurus_file}")
@@ -50,44 +65,44 @@ def generate(pairs, min_threshold=2, max_threshold=5):
         # carte_mere_idx = pivot_cog_map(carte_mere)
 
         for (carte, name) in [(carte_cog, "bag"), (carte_mere, "bag_mere")]:
-            for threshold in range(min_threshold, max_threshold + 1):
-                cooc_matrix = compute_cooc_matrix(carte, min_cooc_threshold=threshold)
-                # pprint(cooc_matrix)
-                cooc_diagonal = {word: cooc_matrix[word][word] for word in cooc_matrix}
-                # version étdenu avec un niveau suppl de dict pour "weight"
-                cooc_matrix_ext = {
-                    row_word: {
-                        col_word: {"weight": cooc_matrix[row_word][col_word]} for col_word in cooc_matrix[row_word]
-                    }
-                    for row_word in cooc_matrix
-                }
-                # on charge dans networkx
-                cooc_graph = nx.Graph(cooc_matrix_ext)
-                # virer les arcs boucles et les isolés
-                cooc_graph.remove_edges_from(nx.selfloop_edges(cooc_graph))
-                cooc_graph.remove_nodes_from(list(nx.isolates(cooc_graph)))
-                # pour les noeuds, le poid c'est le nombre de cartes
-                # PAS fait par la diagonale de cooc_matrix
-                # nx.set_node_attributes(cooc_graph, {w: len(l) for w, l in idx_map.items()}, name="weight")
-                nx.set_node_attributes(cooc_graph, cooc_diagonal, name="weight")
+            for threshold in thresholds:
+                for width in max_window_widths:
+                    cooc_matrix = compute_cooc_matrix(carte, min_cooc_threshold=threshold, max_window_width=width)
+                    # pprint(cooc_matrix)
+                    cooc_diagonal = {word: cooc_matrix[word][word] for word in cooc_matrix}
+                    # version étdenu avec un niveau suppl de dict pour "weight"
+                    cooc_matrix_ext = extend_matrix_to_nx(cooc_matrix)
+                    # on charge dans networkx
+                    cooc_graph = nx.Graph(cooc_matrix_ext)
+                    # virer les arcs boucles et les isolés
+                    cooc_graph.remove_edges_from(nx.selfloop_edges(cooc_graph))
+                    cooc_graph.remove_nodes_from(list(nx.isolates(cooc_graph)))
+                    # pour les noeuds, le poid c'est le nombre de cartes
+                    # PAS fait par la diagonale de cooc_matrix
+                    # nx.set_node_attributes(cooc_graph, {w: len(l) for w, l in idx_map.items()}, name="weight")
+                    nx.set_node_attributes(cooc_graph, cooc_diagonal, name="weight")
 
-                # on génère au format graphml et graphviz
-                filename = f"{GRAPH_DIR / Path(carte_file).stem}_{name}_{threshold}"
-                logger.info(f"  drawing {filename}")
-                logger.info(f"  G:{cooc_graph.number_of_nodes()} mots et {cooc_graph.number_of_edges()} arcs")
-                nx.write_graphml(cooc_graph, f"{filename}.graphml")
-                draw_graphviz(
-                    cooc_graph,
-                    f"{filename}.{IMG_FORMAT}",
-                    algorithm="sfdp",
-                    sep=0.01,
-                    fontsize="proportional",
-                    node_color="weight",
-                    min_edge_penwidths=1,
-                    max_edge_penwidths=10,
-                    min_node_size=0.02,
-                    max_node_size=1,
-                )
+                    # on génère au format graphml et graphviz
+                    filename = f"{GRAPH_DIR / Path(carte_file).stem}_{name}_{threshold}_{width}"
+                    logger.info(f"  drawing {filename}")
+                    logger.info(f"  G:{cooc_graph.number_of_nodes()} mots et {cooc_graph.number_of_edges()} arcs")
+                    if cooc_graph.number_of_nodes() == 0:
+                        logger.warning(f"empty graph {filename}")
+                        continue
+                    # else
+                    nx.write_graphml(cooc_graph, f"{filename}.graphml")
+                    draw_graphviz(
+                        cooc_graph,
+                        f"{filename}.{IMG_FORMAT}",
+                        algorithm="sfdp",
+                        sep=0.01,
+                        fontsize=8,  # "proportional",
+                        node_color="weight",
+                        min_edge_penwidths=1,
+                        max_edge_penwidths=10,
+                        min_node_size=0.02,
+                        max_node_size=1,
+                    )
 
 
 # dossier et format de sortie
@@ -99,7 +114,9 @@ THRESHOLD = 3
 # les deux paire "la mine" et "la mine dans le futur"
 DATASETS = [(CARTES_COG_LA_MINE, THESAURUS_LA_MINE), (CARTES_COG_MINE_FUTUR, THESAURUS_MINE_FUTUR)]
 
+
 if __name__ == "__main__":
-    generate(DATASETS, 2, 10)
+    # [("input/cartes_cog_small.csv", THESAURUS_LA_MINE)]
+    generate(DATASETS, thresholds=[2, 5, 10], max_window_widths=[1, 2, 3, 4, 5])
     # nx.write_graphml(G, GRAPH_DIR / "network.graphml")
     # draw_graphviz(G, GRAPH_DIR / f"network.{IMG_FORMAT}", algorithm="sfdp", sep=0.01, fontsize = "proportional", node_color="weight", min_edge_penwidths=0.01, max_edge_penwidths=4, min_node_size=0.05, max_node_size=2)
