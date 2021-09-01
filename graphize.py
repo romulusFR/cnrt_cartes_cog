723b2551f7b039f06242e2797e9a0afe00942cc7
Romuald THION <romuald.thion@univ-nc.nc>
2021-09-01 18:36:58 +1100

    comments

--- graphize.py
+++ graphize.py
@@ -26,13 +26,11 @@ from draw_graphviz import draw_graphviz
 logger = logging.getLogger(f"COGNITIVE_MAP.{__name__}")
 if __name__ == "__main__":
     logging.basicConfig()
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
 COG_MAP = get_cog_maps(CARTES_COG_LA_MINE)
 ONTOLOGY = get_ontology(THESAURUS_LA_MINE)
 CONCEPT_MAP, EERROR_MAP = apply_ontology(COG_MAP, ONTOLOGY, with_unknown=False)
-
-logger.debug(f"CONCEPT_MAP = {CONCEPT_MAP}")
 
 # dossier et format de sortie
 IMG_FORMAT = "svg"
@@ -40,44 +38,47 @@ GRAPH_DIR = Path("graphs/")
 Path(GRAPH_DIR).mkdir(parents=True, exist_ok=True)
 
 # on pivote CONCEPT_MAP en CONCEPT_IDX
+# à chaque mot les id des cartes où il apparait
 CONCEPT_IDX = defaultdict(list)
-for i, l in CONCEPT_MAP.items():
-    for w in l:
-        CONCEPT_IDX[w].append(i)
+for identifier, words in CONCEPT_MAP.items():
+    for word in words:
+        CONCEPT_IDX[word].append(identifier)
 
-
-logger.info(f"{len(CONCEPT_IDX)} concepts")
+logger.debug(f"CONCEPT_MAP = {CONCEPT_MAP}")
 logger.debug(f"CONCEPT_IDX = { {w:len(l) for w, l in CONCEPT_IDX.items()} }")
+logger.info(f"{len(CONCEPT_IDX)} concepts")
 
-# %%
+
+# dictionnaire des co-occurrences :
+# à chaque mot (lignes)
+#   -> un dictionnaire qui à chaque mot colonne
+#       -> un dictionnaire qui à chaque attribut
+#           -> donne dans "weight" le nombre de cartes où on apparait en commun
+# un dictionnaire de plus que nécessaire pour être compatible avec l'API networkx
 CONCEPT_COOC = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))  # type: ignore
 
-THRESHOLD = 2
-# matrix 49 x 49 = 2401
-for w1 in CONCEPT_IDX:
-    for w2 in CONCEPT_IDX:
-        # inter = CONCEPT_IDX[w1], CONCEPT_IDX[w2]
-        common_times = sum((Counter(CONCEPT_IDX[w1]) & Counter(CONCEPT_IDX[w2])).values())
-        if common_times >= THRESHOLD and w1 != w2:
-            CONCEPT_COOC[w1][w2]["weight"] = common_times
+# nombre minimal de cooc
+THRESHOLD = 5
 
-# pprint(CONCEPT_COOC)
+for word_row in CONCEPT_IDX:
+    for word_col in CONCEPT_IDX:
+        common_times = sum((Counter(CONCEPT_IDX[word_row]) & Counter(CONCEPT_IDX[word_col])).values())
+        if common_times >= THRESHOLD and word_row != word_col:
+            CONCEPT_COOC[word_row][word_col]["weight"] = common_times
 
-logger.debug(f"CONCEPT_IDX['nature'] = {CONCEPT_IDX['nature']}")
-for i in CONCEPT_IDX["nature"]:
-    logger.debug(f"CONCEPT_MAP[{i}] = {CONCEPT_MAP[i]}")
-logger.debug(f"CONCEPT_COOC['nature'] = {CONCEPT_COOC['nature']}")
+# logger.debug(f"CONCEPT_IDX['nature'] = {CONCEPT_IDX['nature']}")
+# for i in CONCEPT_IDX["nature"]:
+#     logger.debug(f"CONCEPT_MAP[{i}] = {CONCEPT_MAP[i]}")
+# logger.debug(f"CONCEPT_COOC['nature'] = {CONCEPT_COOC['nature']}")
 
 
-# %%
 
-
+# on charge dans graphviz
 G = nx.Graph(CONCEPT_COOC)
+# pour les noeuds, le poid c'est le nombre de cartes
 nx.set_node_attributes(G, {w: len(l) for w, l in CONCEPT_IDX.items()}, name="weight")
 
 logger.info(nx.info(G))
-nx.write_graphml(G, GRAPH_DIR / "network.graphml")
-
-
+# nx.write_graphml(G, GRAPH_DIR / "network.graphml")
 if __name__ == "__main__":
     draw_graphviz(G, GRAPH_DIR / f"network.{IMG_FORMAT}", algorithm="sfdp", sep=0.01, fontsize = "proportional", node_color="weight", min_edge_penwidths=0.01, max_edge_penwidths=4)
