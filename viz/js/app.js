624e1d1cab6d4572a47b23b8c0caf66f54196ac8
Romuald Thion <romulusFR@users.noreply.github.com>
2021-09-24 17:43:22 +1100

    reorg demo vizu

---
+++ viz/js/app.js
@@ -0,0 +1,16 @@
+// "https://vega.github.io/vega/examples/bar-chart.vg.json"
+
+const url = "specs/vega-tree.json";
+fetch(url)
+    .then((res) => res.json())
+    .then((spec) => render(spec))
+    .catch((err) => console.error(err));
+
+function render(spec) {
+    const view = new vega.View(vega.parse(spec), {
+        renderer: "svg", // renderer (canvas or svg)
+        container: "#tree-view", // parent DOM container
+        hover: false, // enable hover processing
+    });
+    return view.runAsync();
+}
